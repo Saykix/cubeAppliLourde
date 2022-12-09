4ace1f2b681a9200b52a85f4dd332f5efcd78e6b
Sayki <45237935+Saykix@users.noreply.github.com>
2022-12-09 20:12:11 +0100

    project initialization
    Signed-off-by: Sayki <45237935+Saykix@users.noreply.github.com>

---
+++ cube/src/controllers/Accueil.java
@@ -0,0 +1,75 @@
+package controllers;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+
+public class Accueil {
+	
+	private Parent fxml;
+	
+    @FXML
+    private Text CoutStockage;
+
+    @FXML
+    private Button Inventoriser;
+
+    @FXML
+    private BorderPane accueilPane;
+
+    @FXML
+    private Text commanderVin;
+
+    @FXML
+    private Text coutMarchandise;
+
+    @FXML
+    private Text gererClient;
+
+    @FXML
+    private Text gererStock;
+
+    @FXML
+    private TextField rechercheBare;
+
+    @FXML
+    private TableView<?> tableInventaire;
+
+    @FXML
+    private Text totalProduit;
+
+    @FXML
+    void doInventaire(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goClient(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goStock(MouseEvent event) {
+    	try {
+            fxml = FXMLLoader.load(getClass().getResource("/interfaces/Articles.fxml"));
+            accueilPane.getChildren().removeAll();
+            accueilPane.getChildren().setAll(fxml);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } 
+    }
+
+    @FXML
+    void goVin(MouseEvent event) {
+
+    }
+
+}
